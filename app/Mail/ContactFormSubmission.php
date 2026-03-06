a0cf4e839d12fd9991cdef28bc46b64fd8f094a8
SamZaro <samir.zarouali@gmail.com>
2026-03-06 20:25:13 +0100

    FIX: ShouldQueue verwijderd uit contactform
    ShouldQueue, Queueable en SerializesModels zijn verwijderd. De email wordt nu
      synchroon verstuurd wanneer het contactformulier wordt ingestuurd. Reden is gedoe met Queue workers op live server.

--- app/Mail/ContactFormSubmission.php
+++ app/Mail/ContactFormSubmission.php
@@ -2,18 +2,14 @@
 
 namespace App\Mail;
 
-use Illuminate\Bus\Queueable;
-use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Mail\Mailable;
 use Illuminate\Mail\Mailables\Address;
 use Illuminate\Mail\Mailables\Attachment;
 use Illuminate\Mail\Mailables\Content;
 use Illuminate\Mail\Mailables\Envelope;
-use Illuminate\Queue\SerializesModels;
 
-class ContactFormSubmission extends Mailable implements ShouldQueue
+class ContactFormSubmission extends Mailable
 {
-    use Queueable, SerializesModels;
 
     /**
      * Create a new message instance.
